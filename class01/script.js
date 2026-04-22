3921fe0815c5292fa33df2ac85d819a956090ae8
Jenea-G <eugeniagurulea@gmail.com>
2026-04-22 15:19:26 -0400

    add array, loops and higher order functions

--- class01/script.js
+++ class01/script.js
@@ -60,7 +60,7 @@ const categories = ["movies", "books"]; // this is not going to change, we can a
 
 // we take an argument/parameter in this function
 // this is a void function it executes smth but doesnt return anything and couldnt be assigned to a variable
-function greetUser(name){
+function greetUser(name) {
     console.log("Hello, " + name + "! I hope you are good.")
 }
 
@@ -68,7 +68,7 @@ const name = "Nora";
 greetUser(name);
 
 // return fucntion, executes smth and returnes a value, can be assigned to a variable
-function add(a, b){
+function add(a, b) {
     return a + b;
 }
 const result = add(3, 5);
@@ -83,14 +83,114 @@ const fullName = addName(firstName, lastName);
 
 // 1.
 // return function that adds name and returns it to full name
-function addName(firstName, lastName){
+function addName(firstName, lastName) {
     return `${firstName} ${lastName}`;
 }
 
 // 2.
 // greet the user with fullName
-function greet(){
-    const name = fullName;
+function greet(name) {
     console.log("Hello " + name + "!");
 }
-greet();
+greet(fullName);
+
+// DOM
+document.getElementById("title").innerText = greet(fullName);
+
+/**
+ * One responsibility per function!
+ */
+
+function calculateTotal(price, quantity) {
+    return price * quantity;
+}
+
+function formatPrice(amount) {
+    return "$" + amount.toFixed(2);
+}
+
+const totalPrice = calculateTotal(20.99, 3)
+
+console.log(totalPrice);
+console.log(formatPrice(totalPrice));
+
+/**
+ * array and loops
+ */
+
+const fruits = ["apple", "banana", "orange", "cherry"];
+
+console.log(fruits);
+console.log(fruits[0]); // first element
+console.log(fruits[(fruits.length - 1)]) // last element
+
+console.log("===========");
+console.log("with lambda:");
+fruits.forEach((fruit) => console.log(fruit));
+
+console.log("===========");
+console.log("with callback function:");
+fruits.forEach(function (fruit) { console.log(fruit); });
+
+console.log("===========");
+console.log("with for loop");
+for (const fruit of fruits) {
+    console.log(fruit);
+}
+
+console.log("===========");
+console.log("with indexes loop");
+for (let i = 0; i < fruits.length; i++) {
+    if (i >= 1) { // optional if we dont want to start from the first one
+        console.log(fruits[i]);
+    }
+}
+
+console.log("===========");
+console.log("with while loop");
+let index = 0;
+while (index < fruits.length) {
+    console.log(fruits[index]);
+    index++;
+}
+
+/**
+ * find function
+ */
+
+const prices = [10, 20, 30, 40];
+
+const firstBigPrice = prices.find(function (price) {
+    return price > 25;
+});
+console.log(firstBigPrice);
+
+const expensivePrices = prices.filter(function (price) {
+    return price > 25;
+});
+console.log(expensivePrices);
+
+/**
+ * Map function
+ */
+
+const formattedPrices = prices.map(function (price) {
+    return "$" + price;
+});
+console.log(formattedPrices)
+
+/**
+ * Objects
+ */
+
+const product = {
+    name: "Keyboard",
+    price: 49.99,
+    inStock: true,
+    describe: function () {
+        return this.name + " costs $" + this.price;
+    },
+};
+
+console.log(product.name);
+console.log(product.describe());
