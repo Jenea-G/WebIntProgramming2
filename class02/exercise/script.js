84c43ac7ac14a3efb33af701c526c853e9d63791
Jenea-G <eugeniagurulea@gmail.com>
2026-04-23 15:14:23 -0400

    add exercise prof version

--- class02/exercise/script.js
+++ class02/exercise/script.js
@@ -9,13 +9,28 @@ const greetBtn = document.getElementById("greet-button")
 const output = document.getElementById("output")
 
 // 2. add an event listener to the button
+// greetBtn.addEventListener("click", () => {
+//     const content = nameInput.value.trim()
+
+//     if (content === "") {
+//         output.textContent = "Please enter something";
+//     } else {
+//         setTimeout(() => {
+//             output.textContent = `Hello ${content} !`;
+//         }, 1000);
+//     }
+// });
+
 greetBtn.addEventListener("click", () => {
     const content = nameInput.value.trim()
+
     if (content === "") {
         output.textContent = "Please enter something";
-    } else {
-        setTimeout(() => {
-            output.textContent = `Hello ${content} !`;
-        }, 1000);
+        return; // gets us out of the function
     }
-})
+
+    setTimeout(() => {
+        output.textContent = `Hello ${content} !`;
+    }, 1000);
+});
+
