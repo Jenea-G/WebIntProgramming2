952c291ccd6afb12f29177fdfa98f5fe3f89c2e1
Jenea-G <eugeniagurulea@gmail.com>
2026-04-30 13:31:00 -0400

    class4 chains in promises

---
+++ class04/script.js
@@ -0,0 +1,58 @@
+// Promises and js objects
+
+let student = {
+    id: 1, name: "Alice", role: "Student"
+};
+
+console.log(student);
+
+function loadUserData() {
+    let promise = new Promise((resolve, reject) => {
+        if (auth === true) {
+            resolve(student);
+        } else {
+            reject("")
+        }
+    })
+    return promise;
+}
+
+loadUserData()
+    .then((user) => {
+        console.log(user);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+// API --> Application Programming Interface
+// API : a set of rules and protocols that allow different software appllications to communicate with one another.
+
+// An important aspect of why we use Promises :
+// The term --> concurrency.
+// Modern computers can run multiple code at the same time!
+
+// Promises wrap these eventual outcomes for us, so that we don't have to wait until the outcome is present.
+
+/**
+ * Chains in Promises!
+ */
+
+Promise.resolve(5).then((result) => {
+    // console.log(result * 2);
+    console.log(`Step 1 ${result}`)
+    return result * 2;
+}).then((result) => {
+    result + 20
+    console.log(`Step 2 ${result}`)
+    return result;
+}).then((result) => {
+    let newResult = result * 100
+    console.log(`Step 3 ${newResult}`)
+})
+
+// Let's try with our student.
+
+loadUserData().then((user) => {
+    console.log(user);
+});
