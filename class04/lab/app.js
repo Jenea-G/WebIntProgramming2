2f11298641dca586ae23a482c7efc411cfd77128
Jenea-G <eugeniagurulea@gmail.com>
2026-05-01 19:59:01 -0400

    add getCoursesData function and renderCourses function

--- class04/lab/app.js
+++ class04/lab/app.js
@@ -53,3 +53,54 @@ sBtn.addEventListener("click", () => {
       pStatus.textContent = error;
     });
 });
+
+// =======================
+
+const courses = [
+  { code: "WIP2", title: "Web Interface Programming 2" },
+  { code: "AWP", title: "Advanced Programming" },
+  { code: "DB2", title: "Database Management Systems 2" },
+];
+
+function getCourseData() {
+  const promise = new Promise((resolve, reject) => {
+    if (courses) {
+      setTimeout(() => {
+        resolve(renderCourses(courses));
+      }, 2000);
+    } else {
+      setTimeout(() => {
+        reject("Courses data not found.");
+      }, 2000);
+    }
+  });
+  return promise;
+}
+
+function renderCourses(courses) {
+  const container = document.getElementById("courses-container");
+  const title = document.createElement("h2");
+  title.textContent = "Courses Data";
+  const ulWrap = document.createElement("ul");
+
+  let i = 1;
+  for (const course of courses) {
+    const keys = Object.keys(course);
+    const liInner = document.createElement("li");
+    const ulInner = document.createElement("ul");
+    liInner.textContent = `Course ${i}`;
+    ulWrap.append(liInner);
+    liInner.append(ulInner);
+
+    for (const key of keys) {
+      const li = document.createElement("li");
+      li.textContent = `${key.toUpperCase()} : ${course[key]}`;
+      ulInner.append(li);
+    }
+    i++;
+  }
+
+  container.append(title, ulWrap);
+}
+
+renderCourses(courses);
