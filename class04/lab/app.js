bf084bd0732e8a47e8f2fd08ea25f724ff8bc278
Jenea-G <eugeniagurulea@gmail.com>
2026-05-05 13:18:03 -0400

    lab1 program refactor into main()

--- class04/lab/app.js
+++ class04/lab/app.js
@@ -1,12 +1,66 @@
-const student1 = {
-  id: 1,
-  name: "Kate",
-  programm: "Web design",
-  semester: 3,
-  bio: "personal info",
-};
+function main() {
+  const student1 = {
+    id: 1,
+    name: "Kate",
+    programm: "Web design",
+    semester: 3,
+    bio: "personal info",
+  };
 
-function getStudentData() {
+  const courses = [
+    { code: "WIP2", title: "Web Interface Programming 2" },
+    { code: "AWP", title: "Advanced Programming" },
+    { code: "DB2", title: "Database Management Systems 2" },
+  ];
+
+  const sContainer = document.getElementById("student-container");
+  const cContainer = document.getElementById("courses-container");
+  const sBtn = document.getElementById("load-student-btn");
+  const pStatus = document.getElementById("status");
+  const cBtn = document.getElementById("load-courses-btn");
+  const clearBtn = document.getElementById("clear-btn");
+
+  /** Load student data on button click */
+  sBtn.addEventListener("click", () => {
+    pStatus.textContent = "Loading student data...";
+
+    getStudentData(student1)
+      .then((result) => {
+        result;
+      })
+      .then(() => {
+        pStatus.textContent = "Student data is loaded.";
+      })
+      .catch((error) => {
+        pStatus.textContent = error;
+      });
+  });
+
+  /** Load courses data on button click; */
+  cBtn.addEventListener("click", () => {
+    pStatus.textContent = "Loading courses data...";
+
+    getCoursesData(courses)
+      .then((result) => {
+        result;
+      })
+      .then(() => {
+        pStatus.textContent = "Courses data is loaded.";
+      })
+      .catch((error) => {
+        pStatus.textContent = error;
+      });
+  });
+
+  /** Clear data on button click; */
+  clearBtn.addEventListener("click", () => {
+    pStatus.textContent = "Ready.";
+    sContainer.replaceChildren();
+    cContainer.replaceChildren();
+  });
+}
+
+function getStudentData(student1) {
   const promise = new Promise((resolve, reject) => {
     if (student1 !== "") {
       setTimeout(() => {
@@ -21,9 +75,8 @@ function getStudentData() {
   return promise;
 }
 
-const sContainer = document.getElementById("student-container");
-
 function renderStudent(student) {
+  const sContainer = document.getElementById("student-container");
   const title = document.createElement("h2");
   title.textContent = "Student Data";
   const ul = document.createElement("ul");
@@ -37,36 +90,7 @@ function renderStudent(student) {
   sContainer.append(title, ul);
 }
 
-// Load student data on button click;
-
-const sBtn = document.getElementById("load-student-btn");
-const pStatus = document.getElementById("status");
-
-sBtn.addEventListener("click", () => {
-  pStatus.textContent = "Loading student data...";
-
-  getStudentData()
-    .then((result) => {
-      result;
-    })
-    .then(() => {
-      pStatus.textContent = "Student data is loaded.";
-    })
-    .catch((error) => {
-      pStatus.textContent = error;
-    });
-});
-
-// =======================
-
-// const courses = "";
-const courses = [
-  { code: "WIP2", title: "Web Interface Programming 2" },
-  { code: "AWP", title: "Advanced Programming" },
-  { code: "DB2", title: "Database Management Systems 2" },
-];
-
-function getCoursesData() {
+function getCoursesData(courses) {
   const promise = new Promise((resolve, reject) => {
     if (courses !== "") {
       setTimeout(() => {
@@ -81,9 +105,8 @@ function getCoursesData() {
   return promise;
 }
 
-const cContainer = document.getElementById("courses-container");
-
 function renderCourses(courses) {
+  const cContainer = document.getElementById("courses-container");
   const title = document.createElement("h2");
   title.textContent = "Courses Data";
   const ulWrap = document.createElement("ul");
@@ -108,31 +131,4 @@ function renderCourses(courses) {
   cContainer.append(title, ulWrap);
 }
 
-// Load courses data on button click;
-
-const cBtn = document.getElementById("load-courses-btn");
-
-cBtn.addEventListener("click", () => {
-  pStatus.textContent = "Loading courses data...";
-
-  getCoursesData()
-    .then((result) => {
-      result;
-    })
-    .then(() => {
-      pStatus.textContent = "Courses data is loaded.";
-    })
-    .catch((error) => {
-      pStatus.textContent = error;
-    });
-});
-
-// Clear button
-
-const clearBtn = document.getElementById("clear-btn");
-
-clearBtn.addEventListener("click", () => {
-  pStatus.textContent = "Ready.";
-  sContainer.replaceChildren();
-  cContainer.replaceChildren();
-});
+main();
