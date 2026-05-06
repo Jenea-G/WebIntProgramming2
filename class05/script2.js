d4279d0fd13e29bb3a1ca5c43f55dc32706019dc
Jenea-G <eugeniagurulea@gmail.com>
2026-05-06 14:59:41 -0400

    add Error handling code

--- class05/script2.js
+++ class05/script2.js
@@ -1,27 +1,44 @@
 // let's load all DOM
-
 const loadUserBtn = document.getElementById("load-user-btn");
 const status = document.getElementById("status");
 const output = document.getElementById("output");
 
+// addEventListener to button
 loadUserBtn.addEventListener("click", () => {
   // first status update
-  status.textContent = "Loading user...";
+  status.textContent = "Loading user..";
   output.innerHTML = "";
 
-  const userFetch = fetch("https://jsonplaceholder.typicode.com/users/300");
+  // now let's fetch
+  const userFetch = fetch("https://jsonplaceholder.typicode.com/users/7");
+  // fetch resolves a Promise for us, and gives a Response object.
 
   userFetch
     .then((response) => {
+      // convert Response object to JSON
+      console.log(response);
+
+      if (response.ok == false) {
+        // return an Error object
+        // goes automatically to catch
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+
+      // return a Response object
       return response.json();
     })
+    // update the output with user name
     .then((data) => {
-      console.log(data);
-      output.textContent = `${data.username}`;
-      status.textContent = "User loaded successfully.";
+      // both of these work data.username
+      output.innerHTML = `${data["username"]}`;
+
+      status.textContent = "User Loaded Successfully.";
     })
-    .cathc((error) => {
-      status.textContent = "Failed to load user";
+    // By DEFAULT:
+    // error here is only reserved for 500 level errors (Server errors)
+    // unless we throw an error.
+    .catch((error) => {
+      status.textContent = "Failed to load user.";
       console.log(error);
     });
 });
