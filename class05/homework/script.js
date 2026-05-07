1403663739181859c978c4f249633d84a761ea28
Jenea-G <eugeniagurulea@gmail.com>
2026-05-07 12:53:19 -0400

    add error handling to homework class05 and post body display and some styling

--- class05/homework/script.js
+++ class05/homework/script.js
@@ -1,4 +1,4 @@
-// fetch a list of 5 users
+// fetch a post
 const loadPostBtn = document.getElementById("load-post-btn");
 const status = document.getElementById("status");
 const output = document.getElementById("output");
@@ -8,19 +8,35 @@ loadPostBtn.addEventListener("click", () => {
   status.textContent = "Loading post...";
   output.innerHTML = "";
 
-  fetch("https://jsonplaceholder.typicode.com/posts/1")
+  fetch("https://jsonplaceholder.typicode.com/posts/11")
     .then((response) => {
+      console.log(response); // to check what i am getting
+
+      if (response.ok === false) {
+        throw new Error(`HTTP error: ${response.status}`); // will go to the .catch
+      }
+
       return response.json();
     })
     .then((json) => {
       setTimeout(() => {
-        output.textContent = json.title;
         console.log(json);
+
+        const h2 = document.createElement("h2");
+        const p = document.createElement("p");
+        h2.textContent = json.id + ". " + json.title;
+        h2.classList.add("text-capitalize", "text-success-subtle");
+        p.textContent = json.body;
+
+        output.append(h2, p);
+
         status.textContent = "The post has been loaded successfully.";
       }, 2000);
     })
     .catch((error) => {
       console.log(error);
-      status.textContent = "Couldn't load post.";
+      setTimeout(() => {
+        status.textContent = "Couldn't load post.";
+      }, 1500);
     });
 });
