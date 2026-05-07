e4d7694a59fe2ea0a1405d618b0ee29cc16a296e
Jenea-G <eugeniagurulea@gmail.com>
2026-05-07 14:39:55 -0400

    correct ex2 and add ex3

--- class06/exercise/exercises.js
+++ class06/exercise/exercises.js
@@ -8,12 +8,16 @@ console.log("Here is the title: " + parsedText.title);
 console.log("Here are the credits: " + parsedText.credits);
 
 console.log("===== Ex2 ===== ");
-
 const course = {
   title: "Advanced Programming",
   credits: 3,
   active: true,
 };
-const courseJson = JSON.stringify(course, null, 2);
+const courseJson = JSON.stringify(course);
 console.log("JSON text: " + courseJson);
 console.log("Type of JSON text: " + typeof courseJson);
+
+console.log("===== Ex3 ===== ");
+
+const courseJsonPretty = JSON.stringify(course, null, 2);
+console.log("JSON text with indentation: " + courseJsonPretty);
