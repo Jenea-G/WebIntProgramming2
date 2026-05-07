abd4de978163734dbd89b642a7a615634f254625
Jenea-G <eugeniagurulea@gmail.com>
2026-05-07 14:38:03 -0400

    add exercise 2

--- class06/exercise/exercises.js
+++ class06/exercise/exercises.js
@@ -8,3 +8,12 @@ console.log("Here is the title: " + parsedText.title);
 console.log("Here are the credits: " + parsedText.credits);
 
 console.log("===== Ex2 ===== ");
+
+const course = {
+  title: "Advanced Programming",
+  credits: 3,
+  active: true,
+};
+const courseJson = JSON.stringify(course, null, 2);
+console.log("JSON text: " + courseJson);
+console.log("Type of JSON text: " + typeof courseJson);
