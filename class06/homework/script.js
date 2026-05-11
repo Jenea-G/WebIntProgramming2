896a2aa0419f3c4c2e49d72d8fdd66f6868bde30
Jenea-G <eugeniagurulea@gmail.com>
2026-05-11 19:43:15 -0400

    add event litener to the button and check the request result

--- class06/homework/script.js
+++ class06/homework/script.js
@@ -1,3 +1,15 @@
 const btn = document.getElementById("load-btn");
 const statusMsg = document.getElementById("status-msg");
 const contentDiv = document.getElementById("content-div");
+
+btn.addEventListener("click", () => {
+  statusMsg.textContent = "User information is loading...";
+  const fetchRequest = fetch("https://jsonplaceholder.typicode.com/users/1");
+  fetchRequest
+    .then((response) => {
+      return response.json();
+    })
+    .then((user) => {
+      console.log(user.name);
+    });
+});
