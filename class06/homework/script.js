732b23026e8a8ae23d707511c8cc1b82d5ebff76
Jenea-G <eugeniagurulea@gmail.com>
2026-05-11 20:06:59 -0400

    add card content

--- class06/homework/script.js
+++ class06/homework/script.js
@@ -4,12 +4,36 @@ const contentDiv = document.getElementById("content-div");
 
 btn.addEventListener("click", () => {
   statusMsg.textContent = "User information is loading...";
+  contentDiv.innerHTML = "";
+
   const fetchRequest = fetch("https://jsonplaceholder.typicode.com/users/1");
   fetchRequest
     .then((response) => {
       return response.json();
     })
     .then((user) => {
-      console.log(user.name);
+      setTimeout(() => {
+        statusMsg.textContent = "User information loaded successfully.";
+        statusMsg.classList += " text-success";
+        console.log(user);
+
+        const card = document.createElement("div");
+        card.classList = "card";
+        const divBody = document.createElement("div");
+        divBody.classList = "card-body";
+        const title = document.createElement("h5");
+        title.classList = "card-title";
+        const text = document.createElement("p");
+        text.classList = "card-text";
+        const ul = document.createElement("ul");
+
+        title.textContent = user.name;
+        ul.innerHTML = `<li>Username: ${user.username}</li><li>Email: ${user.email}</li><li>Phone number: ${user.phone}</li><li>Website: ${user.website}</li><li>City: ${user.address.city}</li>`;
+
+        contentDiv.append(card);
+        card.append(divBody);
+        text.append(ul);
+        divBody.append(title, text);
+      }, 2000);
     });
 });
