1e27379eaefa37e2d66461c866d2d1629d5c3f3c
Jenea-G <eugeniagurulea@gmail.com>
2026-05-14 15:03:59 -0400

    select DOM elements in JS and add setStatus function

--- class08/lab2/script.js
+++ class08/lab2/script.js
@@ -1,1 +1,25 @@
+// connect DOM
+const loadBtn = document.getElementById("load-users");
+const clearBtn = document.getElementById("clear-data");
+const cardsHolder = document.getElementById("cards-holder");
+const statusMsg = document.getElementById("status-msg");
 
+function setStatus(element, status) {
+  if (status === "success") {
+    element.textContent = "Data is loaded successfully!";
+    element.classList += " alert-success";
+    // element.classList.remove("alert-danger", "alert-secondary");
+  } else if (status === "loading") {
+    element.textContent = "Data is loading ...";
+    element.classList += " alert-secondary";
+    // element.classList.remove("alert-danger", "alert-success");
+  } else {
+    element.textContent = "Data loading failed.";
+    element.classList += " alert-danger";
+    // element.classList.remove("alert-success", "alert-secondary");
+  }
+}
+
+loadBtn.addEventListener("click", () => {
+  setStatus(statusMsg, "loading");
+});
