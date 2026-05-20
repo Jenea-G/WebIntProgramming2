0fd3d7bd08d85f11422b1386e465e2c41592b210
Jenea-G <eugeniagurulea@gmail.com>
2026-05-20 15:14:32 -0400

    add challenge1 - export/import users_url

--- class09/api.js
+++ class09/api.js
@@ -1,10 +1,10 @@
+import { USERS_URL } from "./config.js";
+
 export function fetchUsers() {
-  return fetch("https://jsonplaceholder.typicode.com/users").then(
-    (response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-      }
-      return response.json();
-    },
-  );
+  return fetch(USERS_URL).then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  });
 }
