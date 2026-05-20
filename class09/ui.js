d0e1c2e51b105c69d13767f3af0d4b0ec2b40e8e
Jenea-G <eugeniagurulea@gmail.com>
2026-05-20 13:18:04 -0400

    add createUserCard function and some styles

--- class09/ui.js
+++ class09/ui.js
@@ -1,17 +1,25 @@
 export function renderUsers(users, container) {
   container.innerHTML = "";
+  container.classList.add("grid");
 
   users.slice(0, 5).forEach((user) => {
-    const article = document.createElement("article");
-    article.innerHTML = `
-      <h2>${user.name}</h2>
-      <p><strong>Email:</strong> ${user.email}</p>
-      <p><strong>City:</strong> ${user.address.city}</p>
-    `;
+    const article = createUserCard(user);
     container.appendChild(article);
   });
 }
 
 export function clearUsers(container) {
   container.innerHTML = "";
+}
+
+export function createUserCard(user) {
+  const article = document.createElement("article");
+  article.classList.add("card");
+  article.innerHTML = `
+      <h2>${user.name}</h2>
+      <p><strong>Email:</strong> ${user.email}</p>
+      <p><strong>City:</strong> ${user.address.city}</p>
+    `;
+  return article;
+  // return a DOM element
 }
