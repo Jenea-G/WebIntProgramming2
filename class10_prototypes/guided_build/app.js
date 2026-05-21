75f8d9e6e52946fb946f77037c143bd3f3d0afca
Jenea-G <eugeniagurulea@gmail.com>
2026-05-21 14:15:58 -0400

    add run demo functionality

--- class10_prototypes/guided_build/app.js
+++ class10_prototypes/guided_build/app.js
@@ -27,3 +27,30 @@ Book.prototype.returnBook = function () {
 Book.prototype.displayInfo = function () {
   return `${this.title} by ${this.author} | Available: ${this.available}`;
 };
+
+// create two objects
+const book1 = new Book("Clean Code", "Robert C. Martin");
+const book2 = new Book("Eloquent JavaScript", "Marijn Haverbeke", false);
+
+// display the results in the page
+
+const runDemoBtn = document.getElementById("run-demo-btn");
+const output = document.getElementById("output");
+
+runDemoBtn.addEventListener("click", () => {
+  output.innerHTML = `
+    <p>${book1.displayInfo()}</p>
+    <p>${book1.borrow()}</p>
+    <p>${book1.displayInfo()}</p>
+    <hr>
+    <p>${book2.displayInfo()}</p>
+    <p>${book2.returnBook()}</p>
+    <p>${book2.displayInfo()}</p>
+  `;
+});
+
+// Inspect shared behaviour
+
+console.log(Object.getPrototypeOf(book1) === Book.prototype);
+console.log(book1.hasOwnProperty("title"));
+console.log(book1.hasOwnProperty("displayInfo"));
