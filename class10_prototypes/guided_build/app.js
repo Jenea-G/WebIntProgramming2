1c100cfe22aa917dd77c1c29ae6a874bd91878e4
Jenea-G <eugeniagurulea@gmail.com>
2026-05-21 14:10:17 -0400

    add prototype methods

--- class10_prototypes/guided_build/app.js
+++ class10_prototypes/guided_build/app.js
@@ -1,7 +1,29 @@
 // constructor function for Book object
 
-function Book(title, author, available = True) {
+function Book(title, author, available = true) {
   this.title = title;
   this.author = author;
   this.available = available;
 }
+
+// add prototype methods
+
+Book.prototype.borrow = function () {
+  if (this.available) {
+    this.available = false;
+    return `${this.title} has been borrowed.`;
+  }
+  return `${this.title} is already borrowed.`;
+};
+
+Book.prototype.returnBook = function () {
+  if (!this.available) {
+    this.available = true;
+    return `${this.title} has been returned.`;
+  }
+  return `${this.title} is already in my collection, your copy must have been borrowed somewhere else.`;
+};
+
+Book.prototype.displayInfo = function () {
+  return `${this.title} by ${this.author} | Available: ${this.available}`;
+};
