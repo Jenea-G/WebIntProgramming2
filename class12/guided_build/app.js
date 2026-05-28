215342b53cecfca59de4925132b01664b084027a
Jenea-G <eugeniagurulea@gmail.com>
2026-05-28 16:44:25 -0400

    add input id with validation

--- class12/guided_build/app.js
+++ class12/guided_build/app.js
@@ -2,26 +2,50 @@
 const loadPostBtn = document.getElementById("load-post-btn");
 const status = document.getElementById("status");
 const output = document.getElementById("output");
+const inputId = document.getElementById("post-id-input");
 
 loadPostBtn.addEventListener("click", () => {
   status.textContent = "Loading post...";
   output.innerHTML = "";
+  const id = Number(inputId.value);
+  console.log(id);
 
-  fetch("https://jsonplaceholder.typicode.com/posts/10")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((post) => {
-      output.innerHTML = `
+  try {
+    validatePostId(id);
+
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((post) => {
+        output.innerHTML = `
             <h2>${post.title}</h2>
             <p>${post.body}</p>
             `;
-      status.textContent = "Post loaded successfully.";
-    })
-    .catch((error) => {
-      status.textContent = `Failed to load post: ${error.messag}`;
-    });
+        status.textContent = "Post loaded successfully.";
+      })
+      .catch((error) => {
+        status.textContent = `Failed to load post: ${error.message}`;
+      });
+  } catch (error) {
+    status.textContent = `Failed to load post: ${error.message}`;
+  }
 });
+
+// Add synchronous validation
+
+function validatePostId(id) {
+  // throw an error if invalid
+  if (typeof id !== "number" || id <= 0) {
+    throw new Error("Post ID must be a positive number");
+  }
+}
+// testing it with try/catch
+// try {
+//   validatePostId(-1);
+// } catch (error) {
+//   console.log(error.message);
+// }
