7e8efae3d0101e002b637c3990b4166e1342d831
Jenea-G <eugeniagurulea@gmail.com>
2026-06-04 14:35:07 -0400

    add ex1 Book class

--- class13/exercise/script.js
+++ class13/exercise/script.js
@@ -5,6 +5,38 @@
 //  getter summary
 //  getter/setter for pages
 //  static method isValidPageCount()
+console.log("=== Ex.1 ====");
+class Book {
+  constructor(title, pages) {
+    this.title = title;
+    this.pages = pages;
+  }
+
+  get summary() {
+    return `The book '${this.title}' has ${this.pages} pages.`;
+  }
+
+  static isValidPageCount(value) {
+    return value > 0;
+  }
+
+  set pages(value) {
+    if (Book.isValidPageCount(value)) {
+      this.__pages = value;
+    } else {
+      throw new Error("The number of pages should be more than 0");
+    }
+  }
+
+  get pages() {
+    return this.__pages;
+  }
+}
+
+const b1 = new Book("The War and Peace", 500);
+console.log(b1.summary);
+console.log(b1.pages);
+// b1.pages = 0;
 
 // 2.
 // Create a BankAccount class with:
