8d325ec98bb87953297882a20eebd7e486f454e6
Jenea-G <eugeniagurulea@gmail.com>
2026-06-10 14:25:21 -0400

    add render tournament cards

--- class14/app.js
+++ class14/app.js
@@ -1,12 +1,20 @@
 import { fetchTournaments } from "./api.js";
+import { renderTournaments } from "./ui.js";
 
 const loadBtn = document.getElementById("load");
 const status = document.getElementById("status");
+const content = document.getElementById("content");
 
 loadBtn.addEventListener("click", () => {
   status.textContent = "Loading posts";
 
-  fetchTournaments().then((tournaments) => {
-    console.log(tournaments[0].name);
-  });
+  fetchTournaments()
+    .then((tournaments) => {
+      //   console.log(tournaments[0].name);
+      renderTournaments(tournaments, content);
+      status.textContent = "Tournaments loaded";
+    })
+    .catch((error) => {
+      status.textContent = `Failed to load tournaments: ${error.message}`;
+    });
 });
---
+++ class14/ui.js
@@ -0,0 +1,34 @@
+export function renderTournaments(tournaments, container) {
+  container.innerHTML = "";
+  //   container.classList.add("gap-2");
+
+  tournaments.forEach((tournament) => {
+    const article = document.createElement("article");
+    const div = document.createElement("div");
+    const viewBtn = document.createElement("button");
+
+    article.classList.add("col-12", "col-md-4");
+    div.classList.add(
+      "border",
+      "border-primary-subtle",
+      "rounded",
+      "p-3",
+      "mb-3",
+    );
+    viewBtn.classList.add("btn", "btn-primary", "btn-small");
+    div.innerHTML = `
+      <h5 class="text-primary">${tournament.name}</h5>
+      <p><strong>Game title:</strong> ${tournament.game}</p>
+      <p><strong>Entry fee:</strong> ${tournament.entryFee}</p>
+      <p><strong>Max Players:</strong> ${tournament.maxPlayers}</p>
+      <p><strong>Registered Players:</strong> ${tournament.registeredPlayers}</p>
+      <p><strong>Status:</strong> ${tournament.status}</p>
+    `;
+
+    viewBtn.textContent = "View Registrations";
+
+    div.appendChild(viewBtn);
+    article.appendChild(div);
+    container.appendChild(article);
+  });
+}
