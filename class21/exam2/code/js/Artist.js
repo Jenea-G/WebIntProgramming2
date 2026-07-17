40ea7875132bcc8e0e7bfb1d6a45ec9fa59c74ce
Jenea-G <eugeniagurulea@gmail.com>
2026-07-16 21:30:51 -0400

    correct Artist class arguments / properties and getter

--- class21/exam2/code/js/Artist.js
+++ class21/exam2/code/js/Artist.js
@@ -2,13 +2,13 @@ export class Artist {
   //SyntaxError: The requested module './Artist.js' does not provide an export named 'Artist' (at app.js:3:10) == removed 'default' from export to match importing
 
   constructor(id, name, country, genre) {
-    this.id = name;
-    this.artistName = id;
-    this.country = genre;
-    this.genre = country;
+    this.id = id;
+    this.name = name;
+    this.country = country;
+    this.genre = genre;
   }
 
   get displayLabel() {
-    return `${this.artistName} — ` + `${this.genre}`;
+    return `${this.name} — ` + `${this.country}`;
   }
 }
