1990e3405f605730591be2b8c49f2b9ec49e06e6
Jenea-G <eugeniagurulea@gmail.com>
2026-07-23 19:00:41 -0400

    create template and current weather custom element

---
+++ LIA/js/currentWeatherElement.js
@@ -0,0 +1,60 @@
+export class CurrentWeatherElement extends HTMLElement {
+  constructor() {
+    super();
+
+    this.__CurrentWeather = null; // reset the currentWeather instance
+
+    const shadow = this.attachShadow({ mode: "open" });
+
+    const template = document.getElementById("weather-template");
+    const clone = template.content.cloneNode(true);
+
+    shadow.appendChild(clone);
+  }
+
+  set currentWeather(value) {
+    // setter to assign currentWeather instance to the custom element
+    this.__currentWeather = value;
+    this.render();
+  }
+
+  get currentWeather() {
+    return this.__currentWeather;
+  }
+
+  render() {
+    const card = this.shadowRoot.querySelector(".weather-card");
+
+    card.className = "weather-card"; // resets the classes of the card to "weather-card"
+
+    if (this.currentWeather.dayPeriod === "Night") {
+      card.classList.add("night");
+    }
+
+    this.shadowRoot.querySelector(".temperature").textContent =
+      this.currentWeather.formattedTemperature;
+
+    this.shadowRoot.querySelector(".apparent-temperature").textContent =
+      this.currentWeather.formattedApparentTemperature;
+
+    this.shadowRoot.querySelector(".humidity").textContent =
+      this.currentWeather.formattedHumidity;
+
+    this.shadowRoot.querySelector(".precipitation").textContent =
+      this.currentWeather.formattedPrecipitation;
+
+    this.shadowRoot.querySelector(".wind").textContent =
+      this.currentWeather.formattedWind;
+
+    this.shadowRoot.querySelector(".wind-gusts").textContent =
+      this.currentWeather.formattedWindGusts;
+
+    this.shadowRoot.querySelector(".rain").textContent =
+      this.currentWeather.formattedRain;
+
+    this.shadowRoot.querySelector(".showers").textContent =
+      this.currentWeather.formattedShowers;
+  }
+}
+
+customElements.define("weather-element", CurrentWeatherElement);
