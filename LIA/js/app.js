3e116953749831a131fa2b7b59a51e7a79d2d10b
Jenea-G <eugeniagurulea@gmail.com>
2026-07-24 19:05:05 -0400

    document functions

--- LIA/js/app.js
+++ LIA/js/app.js
@@ -9,6 +9,7 @@ const statusMessage = document.getElementById("status");
 const locationFormDiv = document.getElementById("location-form-div");
 const content = document.getElementById("content");
 
+/* get the weather data with provided location and display it on the page */
 async function getData(location) {
   try {
     statusMessage.textContent = "Loading the weather data.";
@@ -17,6 +18,7 @@ async function getData(location) {
 
     console.log(weatherData);
 
+    // create a CurrentWeather instance with required properties from received current weather data
     const weather = new CurrentWeather(
       weatherData.current.temperature_2m,
       weatherData.current.relative_humidity_2m,
@@ -28,6 +30,7 @@ async function getData(location) {
       weatherData.current.rain,
       weatherData.current.showers,
     );
+    // call rendering function to display weather
     renderWeather(weather, content);
 
     statusMessage.textContent = "Weather loaded.";
@@ -36,6 +39,7 @@ async function getData(location) {
   }
 }
 
+/* Initialize weather loading */
 btn.addEventListener("click", async () => {
   try {
     document.body.className = "";
@@ -43,10 +47,14 @@ btn.addEventListener("click", async () => {
     content.innerHTML = "";
     statusMessage.textContent = "Getting your location...";
 
+    // get location from the browser
     const location = await getLocation();
     console.log(location);
 
+    // get and display the weather data
     await getData(location);
+
+    // in case of error or denied location display manual location coordinates input
   } catch (error) {
     statusMessage.textContent = `Your location could not be detected. Enter it manually.`;
 
@@ -54,9 +62,11 @@ btn.addEventListener("click", async () => {
   }
 });
 
+/* Catch the dispatched custom event and load weather from received location data */
 locationFormDiv.addEventListener("location-provided", async (event) => {
   try {
     console.log("location details created");
+    // get and display the weather data with provided location coordinates
     await getData(event.detail);
   } catch (error) {
     statusMessage.textContent =
@@ -64,6 +74,7 @@ locationFormDiv.addEventListener("location-provided", async (event) => {
   }
 });
 
+/* Clear the page */
 resetBtn.addEventListener("click", () => {
   document.body.className = "";
   locationFormDiv.innerHTML = "";
--- LIA/js/ui.js
+++ LIA/js/ui.js
@@ -1,13 +1,15 @@
 import { CurrentWeatherElement } from "./currentWeatherElement.js";
 
+/* Render current weather custom element with the loaded weather data in provided container */
 export function renderWeather(weather, content) {
   content.innerHTML = "";
   const card = document.createElement("weather-element");
-  card.currentWeather = weather;
+  card.currentWeather = weather; // attach CurrentWeather class instance to the custom element
 
   content.appendChild(card);
 }
 
+/* Render location form in provided container and collect input location data */
 export function renderLocationForm(locationFormDiv) {
   locationFormDiv.innerHTML = ""; // to prevent forms adding up
   const form = document.createElement("form");
@@ -25,6 +27,7 @@ export function renderLocationForm(locationFormDiv) {
 
   locationFormDiv.appendChild(form);
 
+  /* Listen to the input submission and get the input data */
   form.addEventListener("submit", (event) => {
     event.preventDefault();
     const latitudeInput = document.getElementById("latitude-input");
@@ -33,6 +36,7 @@ export function renderLocationForm(locationFormDiv) {
       latitude: latitudeInput.value,
       longitude: longitudeInput.value,
     };
+    // custom event stores the recieved location data in event.detail
     form.dispatchEvent(
       new CustomEvent("location-provided", {
         detail: location,
